3f1f04c890f1e222027ba9043e3711bf1191d86d
4lineferreira <alineferreirasnsd@gmail.com>
2024-11-14 13:59:08 -0300

    finalizado

--- src/produto/model/ProdutoConsole.java
+++ src/produto/model/ProdutoConsole.java
@@ -8,10 +8,23 @@ public class ProdutoConsole extends Produto {
 		super(nome, marca, id, valor);
 
 		this.versao = versao;
-
+		
 	}
+	
+	
+	public String getVersao() {
+		return versao;
+	}
+
+
+	public void setVersao(String versao) {
+		this.versao = versao;
+	}
+
+
 	@Override
 	public void Visualizar () {
+		super.Visualizar();
 		System.out.println("Versão: " + this.versao);
 	}
 }
