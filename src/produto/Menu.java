fdd61876d54bd19247db977a8334cb930e06c371
4lineferreira <alineferreirasnsd@gmail.com>
2024-11-14 09:11:42 -0300

    Criação do projeto e likando o projeto ao repository

---
+++ src/produto/Menu.java
@@ -0,0 +1,23 @@
+package produto;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true){
+			
+			System.out.println("\n________________________________");
+			System.out.println("    ");
+			System.out.println("");
+			System.out.println();
+			System.out.println();
+			
+		}
+
+	}
+
+}
